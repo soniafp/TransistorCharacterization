0dc25a22cf92569b1d2be3e5eea81d15ad27cb89
schaed <schae@cern.ch>
2016-01-25 14:12:22 +0100

    =setting for 3nmos2

--- sweep_bothdevice.py
+++ sweep_bothdevice.py
@@ -21,6 +21,9 @@ configuration_file = ''
 if ( transistor_number=='7' and chip_number=='1') or ( transistor_number=='7' and chip_number=='3'):
     with open('config_deviceGate7.yaml', 'r') as file:
         configuration_file = yaml.load(file)
+elif ( transistor_number=='2' and chip_number=='3'):
+    with open('config_deviceGate2.yaml', 'r') as file:
+        configuration_file = yaml.load(file)    
 else:
     with open('config_deviceGate.yaml', 'r') as file:
         configuration_file = yaml.load(file)
